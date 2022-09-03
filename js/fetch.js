128d7756ff2159900cc13de41c746e104deaa73c
Jharna203 <jharnakhatun203@gmail.com>
2022-09-03 15:52:15 +0600

    added display all news by catagory id in news TrustBD

--- js/fetch.js
+++ js/fetch.js
@@ -13,20 +13,20 @@
 const loadNewsCategory = () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     fetch(url)
-    .then(response => response.json())
-    // .then(data => console.log(data.data.news_category[0].category_name))
-    .then(data => displayNewsCategory(data.data.news_category))
+        .then(response => response.json())
+        // .then(data => console.log(data.data.news_category[0].category_name))
+        .then(data => displayNewsCategory(data.data.news_category))
 }
 
 //Display all news category using api
-const displayNewsCategory = (categories) =>{
+const displayNewsCategory = (categories) => {
     const categoryContainer = document.getElementById('categoryList-display');
-  
-    for(const category of categories){
+
+    for (const category of categories) {
         console.log(category.category_name);
         const newsDiv = document.createElement('div');
         newsDiv.className = 'd-inline-block px-4 my-4 text-secondary ';
-        newsDiv.innerHTML =`
+        newsDiv.innerHTML = `
         <p role="button"><a onclick = "loadNewsCategoryID('${category.category_id}')">${category.category_name}<a></p>
         `;
         categoryContainer.appendChild(newsDiv);
@@ -35,44 +35,43 @@ const displayNewsCategory = (categories) =>{
 
 //loading data using all news category_id api 
 const loadNewsCategoryID = (category_id) => {
-    
+
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
     console.log(url);
     fetch(url)
-      .then(response => response.json())
-    //   .then(data => console.log(data.data[0]))
-      .then(data => displayNewsCategoryDetails(data.data))
-  }
+        .then(response => response.json())
+        //   .then(data => console.log(data.data[0]))
+        .then(data => displayNewsCategoryDetails(data.data))
+}
 
 //   Display all news data using by category_id
-  const displayNewsCategoryDetails = (newsCategory) => {
+const displayNewsCategoryDetails = news => {
+    console.log(news);
     const newsContainer = document.getElementById('news-container');
-    for(const news of newsCategory){
-        const newsCategoryDiv = document.createElement('div');
-        newsCategoryDiv.innerHTML = `
-    <div class="row g-4">
+    newsContainer.innerHTML = '';
+    for (const newsId of news) {
+        const newsContainerDiv = document.createElement('div');
+        newsContainerDiv.innerHTML = `
+        <div class="card p-3 my-5 shadow" style="max-width: 100%;">
+        <div class="row gy-4">
     <div class="col-md-3">
-        <img src="${news.thumbnail_url}" alt="...">
+        <img src="${newsId.thumbnail_url}" alt="...">
     </div>
     <div class="col-md-9">
         <div class="card-body">
-            <h5 class="card-title">Card Title</h5>
-            <p class="card-text text-secondary"> is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text text-secondary"> is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit longer.</p>
-
+            <h5 class="card-title">${newsId.title}</h5>
+            <p class="card-text text-secondary">${newsId.details}</p>
             <div class="row align-items-center">
                 <div class="col">
                     <!-------------- writer image,name and date -------------->
-                    <div style=" width: 200px;" class="d-flex align-items-center ">
+                    <div style=" width: 250px;" class="d-flex align-items-center ">
                         <div>
-                            <img class="rounded-3 mw-100" src="images/user.png" alt="">
+                            <img style="width:50px; height: 50px;" class="rounded-circle mw-100" src="${newsId.image_url}" alt="">
                         </div>
 
                         <div class="ms-3">
-                            <p class="mb-0"><strong class="mb-0">Jane Cooper</strong></p>
-                            <p class="mb-0">Jan 10, 2022</p>
+                            <p class="mb-0"><strong class="mb-0">${newsId.author.name?newsId.author.name:'Mr.Writer'}</strong></p>
+                            <p class="mb-0">${newsId.author.published_date?newsId.author.published_date:'No bublished date'}</p>
                         </div>
                     </div>
                 </div>
@@ -80,7 +79,7 @@ const loadNewsCategoryID = (category_id) => {
                     <!-------------- View numbers -------------->
                     <div class="d-flex align-items-center">
                         <div><i class="fa-regular fa-eye"></i></div>
-                        <h6 class="mb-0 ms-2 fw-bold">1.5M</h6>
+                        <h6 class="mb-0 ms-2 fw-bold">${newsId.total_view?newsId.total_view:'0'}M</h6>
                     </div>
 
                 </div>
@@ -104,11 +103,10 @@ const loadNewsCategoryID = (category_id) => {
         </div>
     </div>
 </div>
-          `;
-          newsContainer.appendChild(newsCategoryDiv);
-    
-  
+</div>
+        `;
+        newsContainer.appendChild(newsContainerDiv);
     }
-  }
-
+}
+displayNewsCategoryDetails('');
 loadNewsCategory();
